1127dc75cba379492c50efc84afe2b72a788a210
Martin Schuessler <dev@mschuessler.de>
2021-01-13 11:56:12 +0000

    Added TermMatrix custumization

--- spam_binomial_naive_bayes.r
+++ spam_binomial_naive_bayes.r
@@ -9,8 +9,11 @@ test <- spam[-train.index, ]
 corpus.train <- Corpus(VectorSource(train$message))
 corpus.test <- Corpus(VectorSource(test$message))
 
-dtm.train <- DocumentTermMatrix(corpus.train)
-dtm.test <- DocumentTermMatrix(corpus.test)
+freq <- findFreqTerms(DocumentTermMatrix(corpus.train), lowfreq = 2) #, highfreq = 200
+
+dtm.train <- DocumentTermMatrix(corpus.train, control = list(dictionary = freq))
+dtm.test <- DocumentTermMatrix(corpus.test, control = list(dictionary = freq))
 model <- multinomial_naive_bayes(as.matrix(dtm.train), train$type) #dtm.train
 pred <- predict(model,as.matrix(dtm.test), type ="class")
 print(confusionMatrix(pred, test$type)$overall["Accuracy"])
+length(freq)
